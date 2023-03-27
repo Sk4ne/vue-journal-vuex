da57277a081487553c30b5931a6107581bafb6ce
Spekth <neider86@protonmail.com>
2023-03-27 11:10:02 -0500

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+
+
+export default () => ({
+  isLoading : true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text:'Aute in laboris deserunt minim tempor officia eiusmod labore Lorem quis occaecat non. Sit magna irure labore voluptate veniam sint tempor non et commodo ex pariatur dolor anim. Duis commodo nisi consectetur amet. Et ad occaecat anim incididunt nulla dolor consequat. Esse minim quis quis ut dolor irure magna ullamco culpa velit dolore esse.',
+      picture: null
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text:'Proident pariatur sit irure nisi dolore aute enim occaecat ea eiusmod dolor. Cupidatat occaecat commodo ea et non quis fugiat laborum labore. In non nulla duis veniam.',
+      picture: null
+    },
+    {
+      id: new Date().getTime() + 3000,
+      date: new Date().toDateString(),
+      text:'Sunt ea dolore Lorem in sunt non commodo Lorem aute officia duis culpa laboris. Occaecat irure elit laborum aliquip excepteur ex. Sunt velit laborum ea est ex. Lorem sunt nisi nisi nostrud excepteur culpa cillum laboris sint. Officia eu duis ad nulla. Officia do mollit tempor aute cillum dolore anim reprehenderit. Sunt aliqua ipsum qui officia esse et sit excepteur eiusmod sit laboris laboris.',
+      picture: null
+    }
+  ]
+})
